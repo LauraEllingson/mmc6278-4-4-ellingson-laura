1952067f2fb4883d2186d6df51b259676c1d5668
LauraEllingson <lellingson@ufl.edu>
2024-09-14 22:07:09 -0400

    sql script

--- schema/schema.sql
+++ schema/schema.sql
@@ -2,35 +2,31 @@ CREATE DATABASE IF NOT EXISTS music_db;
 
 USE music_db;
 
+
+-- Create the "artists" table
 CREATE TABLE artists (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(100),
--- create a "name" column that's a varchar of max 100 characters
+  name VARCHAR(100) -- create a "name" column that's a varchar of max 100 characters
 );
 
+-- Create the "albums" table
 CREATE TABLE albums (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
--- create a "name" column that's a varchar of max 100 characters
-name VARCHAR(100),
--- create an "artist_id" column that's an integer
-ARTIST_ID INT,
--- create a "release_year" column that's an integer
-release_year INT,
+  name VARCHAR(100), -- create a "name" column that's a varchar of max 100 characters
+  artist_id INT, -- create an "artist_id" column that's an integer
+  release_year INT, -- create a "release_year" column that's an integer
   FOREIGN KEY (artist_id)
     REFERENCES artists (id)
     ON DELETE CASCADE
 );
 
+-- Create the "songs" table
 CREATE TABLE songs (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
--- create a "name" column that's a varchar of max 100 characters
-name VARCHAR(100),
-track_number INT,
--- create a "track_number" column that's an integer
--- create an "artist_id" column that's an integer
-ARTIST_ID INT,
--- create an "album_id" column that's an integer
-album_id INT,
+  name VARCHAR(100), -- create a "name" column that's a varchar of max 100 characters
+  track_number INT, -- create a "track_number" column that's an integer
+  artist_id INT, -- create an "artist_id" column that's an integer
+  album_id INT, -- create an "album_id" column that's an integer
   FOREIGN KEY (artist_id)
     REFERENCES artists (id)
     ON DELETE CASCADE,
@@ -38,6 +34,7 @@ album_id INT,
     REFERENCES albums (id)
     ON DELETE CASCADE
 );
+
 
 -- Remember to use a comma BETWEEN each line!
 

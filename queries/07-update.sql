c57838a928be7cd53dc2757744211854f1551d06
LauraEllingson <lellingson@ufl.edu>
2024-09-14 23:05:00 -0400

    metallicachange

--- queries/07-update.sql
+++ queries/07-update.sql
@@ -1,2 +1,8 @@
 -- Metallica's Black Album is actually a self-titled album
 -- Change the name of The Black Album to "Metallica"
+UPDATE albums
+SET name = 'Metallica', artist_id = 1
+WHERE id = 3;
+
+
+
